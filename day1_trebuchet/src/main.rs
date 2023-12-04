ee29b839e2a7b8e9a13890bbe69c33a270aee276
Gurwinder Singh <gurwindersi@adobe.com>
2023-12-04 09:19:16 +0530

    refactor

--- day1_trebuchet/src/main.rs
+++ day1_trebuchet/src/main.rs
@@ -1,55 +1,36 @@
 fn main() {
     let input = include_str!("../input.txt");
-    let part_1 = find_calibration(input, part1);
+    let part_1 = find_calibration(input, part1_check_digit);
     println!("Part 1: {}", part_1);
 
-    let part_2 = find_calibration(input, part2);
+    let part_2 = find_calibration(input, part2_check_digit);
     println!("Part 2: {}", part_2);
 }
 
-fn part1(line: &str) -> u32 {
-    let first_digit = line.chars().find(char::is_ascii_digit).unwrap();
-    let last_digit = line.chars().rev().find(char::is_ascii_digit).unwrap();
-    let first_digit = first_digit.to_digit(10).unwrap();
-    let last_digit = last_digit.to_digit(10).unwrap();
-    first_digit * 10 + last_digit
+fn find_calibration(
+    calibration_doc: &str,
+    digit_callback: impl Fn(&str, usize) -> Option<u8>,
+) -> usize {
+    let mut sum = 0;
+    for line in calibration_doc.lines() {
+        sum += calibration_from_line(line, &digit_callback) as usize;
+    }
+    sum
 }
 
-fn part2(line: &str) -> u32 {
-    const WORD_MAP: [&[u8]; 10] = [
-        b"zero", b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
-    ];
-
+fn calibration_from_line(line: &str, digit_callback: impl Fn(&str, usize) -> Option<u8>) -> u32 {
     let mut first_digit = 0;
-    let chars = line.as_bytes();
-    let mut i = 0;
-    'outer: while i < chars.len() {
-        for (j, w) in WORD_MAP.iter().enumerate() {
-            if chars[i..].starts_with(w) {
-                first_digit = j as u32;
-                break 'outer;
-            }
-        }
-        if chars[i].is_ascii_digit() {
-            first_digit = (chars[i] - b'0') as u32;
+    for i in 0..line.len() {
+        if let Some(digit) = digit_callback(line, i) {
+            first_digit = digit as u32;
             break;
         }
-        i += 1;
     }
 
     let mut last_digit = 0;
-    let chars = line.as_bytes();
-    let mut i = chars.len();
-    'outer: while i > 0 {
-        i -= 1;
-        for (j, w) in WORD_MAP.iter().enumerate() {
-            if chars[i..].starts_with(w) {
-                last_digit = j as u32;
-                break 'outer;
-            }
-        }
-        if chars[i].is_ascii_digit() {
-            last_digit = (chars[i] - b'0') as u32;
+    for i in (0..line.len()).rev() {
+        if let Some(digit) = digit_callback(line, i) {
+            last_digit = digit as u32;
             break;
         }
     }
@@ -57,10 +38,28 @@ fn part2(line: &str) -> u32 {
     first_digit * 10 + last_digit
 }
 
-fn find_calibration(calibration_doc: &str, callback: impl Fn(&str) -> u32) -> usize {
-    let mut sum = 0;
-    for line in calibration_doc.lines() {
-        sum += callback(line) as usize;
+fn part1_check_digit(text: &str, index: usize) -> Option<u8> {
+    if let Some(c) = text.as_bytes().get(index) {
+        if c.is_ascii_digit() {
+            return Some(c - b'0');
+        }
     }
-    sum
+    None
+}
+
+fn part2_check_digit(text: &str, index: usize) -> Option<u8> {
+    const WORD_MAP: [&str; 10] = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    if let Some(digit) = part1_check_digit(text, index) {
+        return Some(digit);
+    }
+
+    for (word_index, word) in WORD_MAP.iter().enumerate() {
+        if text[index..].starts_with(word) {
+            return Some(word_index as u8);
+        }
+    }
+    None
 }
