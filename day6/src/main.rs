f516037be9f085d1bd7443076956a90496317822
Gurwinder Singh <gurwindersi@adobe.com>
2023-12-12 10:42:55 +0530

    part 6 day 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     let input = include_str!("../input.txt");
     let output = part_1(input);
     println!("Output: {output}");
+
+    let output = part_2(input);
+    println!("Output: {output}");
 }
 
 fn part_1(input: &str) -> usize {
@@ -16,10 +19,34 @@ fn part_1(input: &str) -> usize {
     total
 }
 
+fn part_2(input: &str) -> usize {
+    let mut lines = input.lines();
+    let time = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Time:")
+        .unwrap()
+        .replace(' ', "")
+        .parse()
+        .unwrap();
+    let distance = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Distance:")
+        .unwrap()
+        .replace(' ', "")
+        .parse()
+        .unwrap();
+    // Time:        51699878
+    // Distance:   377117112241505
+
+    win_combination(time, distance)
+}
+
 fn win_combination(t: usize, s: usize) -> usize {
-    let term = (t.pow(2) as f32 - 4.0 * s as f32).sqrt();
-    let upper = (t as f32 + term) / 2.0;
-    let lower = (t as f32 - term) / 2.0;
+    let term = (t.pow(2) as f64 - 4.0 * s as f64).sqrt();
+    let upper = (t as f64 + term) / 2.0;
+    let lower = (t as f64 - term) / 2.0;
     let upper = (upper - 1.0).ceil();
     let lower = (lower + 1.0).floor();
     (upper - lower + 1.0) as usize
@@ -44,5 +71,11 @@ Distance:  9  40  200";
     fn part1_basic() {
         let output = part_1(INPUT);
         assert_eq!(output, 288);
+    }
+
+    #[test]
+    fn part2_basic() {
+        let output = part_2(INPUT);
+        assert_eq!(output, 71503);
     }
 }
