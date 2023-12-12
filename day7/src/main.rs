3d8f07c18ac4eec881cc7cca7f8db7c4d3f6adda
Gurwinder Singh <gurwindersi@adobe.com>
2023-12-12 12:52:14 +0530

    Day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,14 +1,26 @@
-use std::str::FromStr;
-
 fn main() {
     let input = include_str!("../input.txt");
     let output = part_1(input);
+    println!("Output: {output}");
+
+    let output = part_2(input);
     println!("Output: {output}");
 }
 
 fn part_1(input: &str) -> usize {
     let mut deck = parse_deck(input);
-    deck.sort_by_key(|(hand, _bid)| *hand);
+    deck.sort_by_key(|(hand, _)| *hand);
+
+    let mut total = 0;
+    for (i, (_hand, bid)) in deck.into_iter().enumerate() {
+        total += bid * (i + 1);
+    }
+    total
+}
+
+fn part_2(input: &str) -> usize {
+    let mut deck = parse_deck2(input);
+    deck.sort_by_key(|(hand, _)| *hand);
 
     let mut total = 0;
     for (i, (_hand, bid)) in deck.into_iter().enumerate() {
@@ -23,7 +35,18 @@ fn parse_deck(input: &str) -> Vec<(Hand, usize)> {
 
 fn parse_deck_line(line: &str) -> (Hand, usize) {
     let mut line = line.split_whitespace();
-    let hand = line.next().unwrap().parse().unwrap();
+    let hand = Hand::from_str(line.next().unwrap());
+    let bid = line.next().unwrap().parse().unwrap();
+    (hand, bid)
+}
+
+fn parse_deck2(input: &str) -> Vec<(Hand, usize)> {
+    input.lines().map(parse_deck_line2).collect()
+}
+
+fn parse_deck_line2(line: &str) -> (Hand, usize) {
+    let mut line = line.split_whitespace();
+    let hand = Hand::from_str_part_2(line.next().unwrap());
     let bid = line.next().unwrap().parse().unwrap();
     (hand, bid)
 }
@@ -31,38 +54,76 @@ fn parse_deck_line(line: &str) -> (Hand, usize) {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Hand {
     kind: HandKind,
-    cards: [Card; 5],
+    cards: [u8; 5],
 }
 
 impl Hand {
-    fn kind(cards: [Card; 5]) -> HandKind {
-        let mut map = [0; Card::CardA as usize + 1];
+    fn hand_kind(freq_map: [usize; 6]) -> HandKind {
+        if freq_map[5] == 1 {
+            HandKind::FiveOfAKind
+        } else if freq_map[4] == 1 {
+            HandKind::FourOfAKind
+        } else if freq_map[3] == 1 && freq_map[2] == 1 {
+            HandKind::FullHouse
+        } else if freq_map[3] == 1 {
+            HandKind::ThreeOfAKind
+        } else if freq_map[2] == 2 {
+            HandKind::TwoPair
+        } else if freq_map[2] == 1 {
+            HandKind::OnePair
+        } else {
+            HandKind::HighCard
+        }
+    }
+
+    fn kind(cards: [u8; 5]) -> HandKind {
+        let mut map = [0; 13];
         for c in cards {
             map[c as usize] += 1;
         }
-        let mut map2 = [0; 6];
+        let mut freq_map = [0; 6];
         for v in map {
-            map2[v] += 1;
+            freq_map[v] += 1;
         }
-        if map2[5] == 1 {
+        Self::hand_kind(freq_map)
+    }
+
+    fn kind_part_2(cards: [u8; 5]) -> HandKind {
+        let mut map = [0; 13];
+        for c in cards {
+            map[c as usize] += 1;
+        }
+        let j = map[0];
+        if j == 5 {
             return HandKind::FiveOfAKind;
         }
-        if map2[4] == 1 {
-            return HandKind::FourOfAKind;
+
+        let mut freq_map = [0; 6];
+        for v in map {
+            freq_map[v] += 1;
         }
-        if map2[3] == 1 && map2[2] == 1 {
-            return HandKind::FullHouse;
+        let hand_kind = Self::hand_kind(freq_map);
+        hand_kind.joker_upgrade(j)
+    }
+
+    fn from_str(s: &str) -> Self {
+        let mut cards = [0; 5];
+        let mut chars = s.char_indices();
+        while let Some((i, c)) = chars.next() {
+            cards[i] = parse_card(c);
         }
-        if map2[3] == 1 {
-            return HandKind::ThreeOfAKind;
+        let kind = Hand::kind(cards);
+        Hand { kind, cards }
+    }
+
+    fn from_str_part_2(s: &str) -> Self {
+        let mut cards = [0; 5];
+        let mut chars = s.char_indices();
+        while let Some((i, c)) = chars.next() {
+            cards[i] = parse_card_part_2(c);
         }
-        if map2[2] == 2 {
-            return HandKind::TwoPair;
-        }
-        if map2[2] == 1 {
-            return HandKind::OnePair;
-        }
-        return HandKind::HighCard;
+        let kind = Hand::kind_part_2(cards);
+        Hand { kind, cards }
     }
 }
 
@@ -77,60 +138,66 @@ enum HandKind {
     FiveOfAKind,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
-enum Card {
-    Card2,
-    Card3,
-    Card4,
-    Card5,
-    Card6,
-    Card7,
-    Card8,
-    Card9,
-    CardT,
-    CardJ,
-    CardQ,
-    CardK,
-    CardA,
-}
+impl HandKind {
+    pub fn joker_upgrade(self, jokers: usize) -> Self {
+        if jokers == 0 {
+            return self;
+        }
 
-impl TryFrom<char> for Card {
-    type Error = &'static str;
-
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        use Card::*;
-        let card = match value {
-            '2' => Card2,
-            '3' => Card3,
-            '4' => Card4,
-            '5' => Card5,
-            '6' => Card6,
-            '7' => Card7,
-            '8' => Card8,
-            '9' => Card9,
-            'T' => CardT,
-            'J' => CardJ,
-            'Q' => CardQ,
-            'K' => CardK,
-            'A' => CardA,
-            _ => return Err("Unknown card"),
-        };
-        Ok(card)
+        use HandKind::*;
+        match self {
+            HighCard => OnePair,
+            OnePair => ThreeOfAKind,
+            TwoPair => {
+                if jokers == 2 {
+                    FourOfAKind
+                } else {
+                    FullHouse
+                }
+            }
+            ThreeOfAKind => FourOfAKind,
+            FullHouse => FiveOfAKind,
+            FourOfAKind => FiveOfAKind,
+            FiveOfAKind => FiveOfAKind,
+        }
     }
 }
 
-impl FromStr for Hand {
-    type Err = &'static str;
+fn parse_card(c: char) -> u8 {
+    match c {
+        '2' => 0,
+        '3' => 1,
+        '4' => 2,
+        '5' => 3,
+        '6' => 4,
+        '7' => 5,
+        '8' => 6,
+        '9' => 7,
+        'T' => 8,
+        'J' => 9,
+        'Q' => 10,
+        'K' => 11,
+        'A' => 12,
+        _ => panic!("Unknown card"),
+    }
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut cards = [Card::Card2; 5];
-        let mut chars = s.char_indices();
-        while let Some((i, c)) = chars.next() {
-            let card = Card::try_from(c)?;
-            cards[i] = card;
-        }
-        let kind = Hand::kind(cards);
-        Ok(Hand { kind, cards })
+fn parse_card_part_2(c: char) -> u8 {
+    match c {
+        'J' => 0,
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        '8' => 7,
+        '9' => 8,
+        'T' => 9,
+        'Q' => 10,
+        'K' => 11,
+        'A' => 12,
+        _ => panic!("Unknown card"),
     }
 }
 
@@ -148,5 +215,11 @@ QQQJA 483"#;
     fn part1_basic() {
         let output = part_1(INPUT);
         assert_eq!(output, 6440);
+    }
+
+    #[test]
+    fn part2_basic() {
+        let output = part_2(INPUT);
+        assert_eq!(output, 5905);
     }
 }
