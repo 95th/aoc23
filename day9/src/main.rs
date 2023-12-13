c6e4c3d4cb49049fbe2de2809ca917f0b33a89e7
Gurwinder Singh <gurwindersi@adobe.com>
2023-12-13 14:45:45 +0530

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     let input = include_str!("../input.txt");
     let output = part_1(input);
     println!("Output: {output}");
+
+    let output = part_2(input);
+    println!("Output: {output}");
 }
 
 fn part_1(input: &str) -> isize {
@@ -17,6 +20,21 @@ fn part_1(input: &str) -> isize {
         .unwrap();
 
     predict_next(combined)
+}
+
+fn part_2(input: &str) -> isize {
+    let combined = input
+        .lines()
+        .map(parse_list)
+        .reduce(|a, b| {
+            a.into_iter()
+                .zip(b.into_iter())
+                .map(|(a, b)| a + b)
+                .collect()
+        })
+        .unwrap();
+
+    predict_prev(combined)
 }
 
 fn predict_next(list: Vec<isize>) -> isize {
@@ -46,6 +64,33 @@ fn predict_next(list: Vec<isize>) -> isize {
     curr
 }
 
+fn predict_prev(list: Vec<isize>) -> isize {
+    let mut stack = vec![list];
+
+    loop {
+        let mut new = Vec::new();
+        let last = stack.last().unwrap();
+        let mut zeros = 0;
+        for i in 0..last.len() - 1 {
+            let d = last[i + 1] - last[i];
+            new.push(d);
+            if d == 0 {
+                zeros += 1;
+            }
+        }
+        if zeros == new.len() {
+            break;
+        }
+        stack.push(new);
+    }
+
+    let mut curr = 0;
+    while let Some(list) = stack.pop() {
+        curr = list[0] - curr;
+    }
+    curr
+}
+
 fn parse_list(line: &str) -> Vec<isize> {
     line.split_whitespace()
         .map(|s| s.parse().unwrap())
@@ -63,5 +108,14 @@ mod tests {
 10 13 16 21 30 45"#;
         let output = part_1(input);
         assert_eq!(output, 114)
+    }
+
+    #[test]
+    fn part2_basic() {
+        let input = r#"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"#;
+        let output = part_2(input);
+        assert_eq!(output, 2)
     }
 }
