190a13dd1e6fb78ba797be5fe4e32d1b1c70e8fb
Gurwinder Singh <gurwindersi@adobe.com>
2023-12-20 20:14:12 +0530

    Day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -2,74 +2,82 @@ fn main() {
     let input = include_str!("../input.txt");
     let output = part_1(input);
     println!("{output}");
+    let output = part_2(input);
+    println!("{output}");
 }
 
 fn part_1(input: &str) -> usize {
     let mut total = 0;
     for line in input.lines() {
-        total += get_arrangements(line);
+        let (first, second) = line.split_once(' ').unwrap();
+        let sizes: Vec<_> = second.split(',').map(|it| it.parse().unwrap()).collect();
+        total += get_combinations(first.as_bytes(), &sizes);
     }
     total
 }
 
-fn get_arrangements(row: &str) -> usize {
-    let mut split = row.split(' ');
-    let first = split.next().unwrap();
-    let second = split.next().unwrap();
+fn part_2(input: &str) -> usize {
+    let mut total = 0;
+    for line in input.lines() {
+        let (first, second) = line.split_once(' ').unwrap();
 
-    let sizes: Vec<_> = second.split(',').map(|it| it.parse().unwrap()).collect();
-
-    get_combinations(first.as_bytes(), &sizes, 0, 0)
+        let first = [first; 5].join("?");
+        let sizes = second
+            .split(',')
+            .map(|it| it.parse().unwrap())
+            .collect::<Vec<_>>()
+            .repeat(5);
+        total += get_combinations(first.as_bytes(), &sizes);
+    }
+    total
 }
 
-fn get_combinations(
+fn get_combinations(pattern: &[u8], sizes: &[usize]) -> usize {
+    let mut cache = vec![vec![-1; sizes.len()]; pattern.len()];
+    get_combinations_cached(pattern, sizes, 0, 0, &mut cache)
+}
+
+fn get_combinations_cached(
     pattern: &[u8],
     sizes: &[usize],
-    mut pat_index: usize,
+    pat_index: usize,
     size_index: usize,
+    cache: &mut Vec<Vec<i64>>,
 ) -> usize {
-    while let Some(b'.') = pattern.get(pat_index) {
-        pat_index += 1;
+    match cache.get(pat_index).and_then(|it| it.get(size_index)) {
+        Some(&n) if n >= 0 => return n as usize,
+        _ => (),
     }
 
-    if size_index == sizes.len() {
-        while let Some(b'?' | b'.') = pattern.get(pat_index) {
-            pat_index += 1;
-        }
+    if size_index >= sizes.len() {
+        return if pat_index < pattern.len() && pattern[pat_index..].contains(&b'#') {
+            0
+        } else {
+            1
+        };
     }
 
-    if pat_index >= pattern.len() && size_index >= sizes.len() {
-        return 1;
-    }
-    if pat_index >= pattern.len() || size_index >= sizes.len() {
-        return 0;
-    }
-
-    let size = sizes[size_index];
-    if pat_index + size > pattern.len() {
+    if pat_index >= pattern.len() {
         return 0;
     }
 
     let mut combinations = 0;
-    if pattern[pat_index..][..size]
-        .iter()
-        .all(|&c| c == b'?' || c == b'#')
-    {
-        if let Some(b'.' | b'?') = pattern.get(pat_index + size) {
-            combinations = get_combinations(pattern, sizes, pat_index + size + 1, size_index + 1);
-        } else if let Some(b'#') = pattern.get(pat_index + size) {
-            combinations = 0;
-        } else if size_index == sizes.len() - 1 {
-            combinations = 1;
-        } else {
-            combinations = 0;
+
+    if let b'.' | b'?' = pattern[pat_index] {
+        combinations += get_combinations_cached(pattern, sizes, pat_index + 1, size_index, cache);
+    }
+
+    if let b'#' | b'?' = pattern[pat_index] {
+        let end = pat_index + sizes[size_index];
+        if end <= pattern.len()
+            && !pattern[pat_index..end].contains(&b'.')
+            && (end == pattern.len() || pattern[end] != b'#')
+        {
+            combinations += get_combinations_cached(pattern, sizes, end + 1, size_index + 1, cache)
         }
     }
 
-    if pattern[pat_index] == b'?' {
-        combinations += get_combinations(pattern, sizes, pat_index + 1, size_index);
-    }
-
+    cache[pat_index][size_index] = combinations as i64;
     combinations
 }
 
@@ -96,5 +104,25 @@ mod tests {
         assert_eq!(part_1("??? 2"), 2);
         assert_eq!(part_1("??# 2"), 1);
         assert_eq!(part_1("??#? 2"), 2);
+        assert_eq!(part_1("??.???.???.???.???. 2,2,2"), 56);
+    }
+
+    #[test]
+    fn part_2_basic() {
+        let input = r#"???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"#;
+        let output = part_2(input);
+        assert_eq!(output, 525152);
+    }
+
+    #[test]
+    fn part_2_custom() {
+        assert_eq!(part_2("??? 3"), 1);
+        assert_eq!(part_2("??? 1,1"), 1);
+        assert_eq!(part_2("??. 2"), 16);
     }
 }
