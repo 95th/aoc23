443b2568728847b3b34d90e96281d1e2da321501
Gurwinder Singh <gurwindersi@adobe.com>
2023-12-27 08:00:54 +0530

    Day 20 part 1

---
+++ day20/src/main.rs
@@ -0,0 +1,135 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let output = part_1(input);
+    println!("{output}");
+}
+
+fn part_1(input: &str) -> usize {
+    let mut g = HashMap::<&str, Module>::new();
+    let mut inputs = HashMap::<&str, usize>::new();
+    for line in input.lines() {
+        let (name, rest) = line.split_once(" -> ").unwrap();
+        let out: Vec<_> = rest.split(", ").collect();
+        let (name, kind) = if let Some(name) = name.strip_prefix('%') {
+            (name, ModuleKind::FlipFlop { on: false })
+        } else if let Some(name) = name.strip_prefix('&') {
+            (
+                name,
+                ModuleKind::Conjunction {
+                    high: HashSet::new(),
+                },
+            )
+        } else {
+            (name, ModuleKind::Broadcast)
+        };
+        for o in out.iter() {
+            *inputs.entry(o).or_insert(0) += 1;
+        }
+        g.insert(name, Module { kind, out });
+    }
+
+    let mut low_count = 0;
+    let mut high_count = 0;
+
+    let mut queue = VecDeque::<(&str, &str, Pulse)>::new();
+    for _ in 0..1000 {
+        queue.push_back(("button", "broadcaster", Pulse::Low));
+        while let Some((input, node, pulse)) = queue.pop_front() {
+            match pulse {
+                Pulse::Low => low_count += 1,
+                Pulse::High => high_count += 1,
+            }
+
+            let Some(module) = g.get_mut(node) else {
+                continue;
+            };
+            match &mut module.kind {
+                ModuleKind::Broadcast => {
+                    for n in module.out.iter() {
+                        queue.push_back((node, n, pulse));
+                    }
+                }
+                ModuleKind::FlipFlop { on } => {
+                    if let Pulse::Low = pulse {
+                        *on = !*on;
+                        if *on {
+                            for n in module.out.iter() {
+                                queue.push_back((node, n, Pulse::High));
+                            }
+                        } else {
+                            for n in module.out.iter() {
+                                queue.push_back((node, n, Pulse::Low));
+                            }
+                        }
+                    }
+                }
+                ModuleKind::Conjunction { high } => {
+                    if let Pulse::High = pulse {
+                        high.insert(input);
+                    } else {
+                        high.remove(input);
+                    }
+                    if high.len() == inputs[node] {
+                        for n in module.out.iter() {
+                            queue.push_back((node, n, Pulse::Low));
+                        }
+                    } else {
+                        for n in module.out.iter() {
+                            queue.push_back((node, n, Pulse::High));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    low_count * high_count
+}
+
+#[derive(Debug)]
+struct Module<'a> {
+    kind: ModuleKind<'a>,
+    out: Vec<&'a str>,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Pulse {
+    Low,
+    High,
+}
+
+#[derive(Debug)]
+enum ModuleKind<'a> {
+    Broadcast,
+    FlipFlop { on: bool },
+    Conjunction { high: HashSet<&'a str> },
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_basic() {
+        let input = r"broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a";
+        let output = part_1(input);
+        assert_eq!(output, 32000000);
+    }
+
+    #[test]
+    fn part_1_interesting() {
+        let input = r"broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output";
+        let output = part_1(input);
+        assert_eq!(output, 11687500);
+    }
+}
